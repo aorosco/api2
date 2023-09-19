1e7b0a9c10d0aeddd05184430f234b40a4ee4f81
aorosco <oroscoalejandro@gmail.com>
2023-09-19 01:34:36 -0400

    detalles en index  revisados

--- src/index.js
+++ src/index.js
@@ -9,12 +9,13 @@ import './models/relaciones.js'
 */
 async function main(){
     try {
+        console.log("----- Consulta de prueba para verificar conexion  ------ ")
         await sequelize.authenticate();
         ///await sequelize.sync({force:true});
         ///await sequelize.sync();
-        console.log("conexion exitosa")
-        app.listen(3333)
-        console.log("server corriendo en al puerto, 3333")
+        console.log("----- conexion exitosa a la base de datos API2 ------ ")
+        app.listen(7000)
+        console.log("server corriendo en al puerto, 7000")
     } catch (error) {
         console.error("no se pudo conectar a la base de datos: ",error)
     }
